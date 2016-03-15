50ba65766fb48b64e110e15e5c2505bb3fca68c1
Tong <tong@disktree.net>
2016-03-15 07:50:27 +0100

    ...

--- src/om/Term.hx
+++ src/om/Term.hx
@@ -1,48 +1,195 @@
 package om;
 
-import om.term.ANSI;
+#if (sys||nodejs)
 
-@:require(sys)
+import om.term.ANSI;
+import om.term.ANSI.BELL;
+import om.term.ANSI.CSI;
+import om.term.ANSI.ESCAPE;
+import om.term.ANSI.SUFFIX;
+
 class Term {
 
-    /**
-        Try to guess if your terminal supports unicode.
-    */
-    public static function hasUnicode() : Bool {
-        switch Sys.systemName() {
-        case 'Windows_NT':
-            return false;
-        default:
-            if( Sys.getEnv( 'LC_CTYPE' ) == 'UTF-8' )
-                return true;
-            var isUTF8 = ~/[.]UTF-8/;
-            var LC_ALL = Sys.getEnv( 'LC_ALL' );
-            if( LC_ALL != null && isUTF8.match( LC_ALL ) )
-                return true;
-            var LANG = Sys.getEnv( 'LANG' );
-            if( LANG != null && isUTF8.match( LANG ) )
-                return true;
-        }
-        return false;
+	//var _color : Color;
+	//var _background : BackgroundColor;
+
+	public function new() {
+	}
+
+	public inline function csi( str : String ) : Term {
+		print( CSI + str );
+		return this;
+	}
+
+	/*
+	public inline function set( code : Int ) {
+		print( CSI + code );
+		return this;
+    }
+	*/
+
+	public inline function beep() : Term {
+        return print( '\x07' );
     }
 
-    public static inline function beep() {
-        Sys.stdout().writeString( ANSI.BELL );
+	public inline function title( str : String ) : Term {
+		return print( ESCAPE + ']0;' + str + BELL );
+	}
+
+	/// --- Cursor Position
+
+	public inline function up( n = 0 ) : Term {
+		return csi( Std.string(n) + Control.up );
+	}
+
+	public inline function down( n = 0 ) : Term {
+		return csi( Std.string(n) + Control.down );
+	}
+
+	public inline function back( n = 0 ) : Term {
+		return csi( Std.string(n) + Control.back );
+	}
+
+	public inline function forward( n = 0 ) : Term {
+		return csi( Std.string(n) + Control.forward );
+	}
+
+	public inline function nextLine() : Term {
+		return csi( Control.nextLine );
+	}
+
+	public inline function previousLine() : Term {
+		return csi( Control.previousLine );
+	}
+
+	/// ---
+
+	/*
+	public inline function eraseChar() : Term {
+        return csi( '0K' );
+		return this;
+    }
+	*/
+
+	public inline function clear() : Term {
+		return print( '\033c' );
+	}
+
+	/// --- Colors
+
+	/*
+	public inline function rgb( r : Int, g : Int, b : Int ) {
+		print( CSI + '38;2;'+'$r;$g;$b'+'m' );
+		return this;
+	}
+	*/
+
+	public function setColor( code : Int ) : Term {
+		return csi( code + 'm' );
+	}
+
+	public inline function fg( color : Int = null ) : Term {
+		//print( CSI + '38;5;' + color + 'm' );
+		return setColor( color );
     }
 
-    public static function clearLine() {
-        Sys.stdout().writeString( '\x07' );
+	public inline function bg( color : Int = null ) : Term {
+		return setColor( color );
     }
 
-    public static function setTitle( text : String ) {
-        Sys.stdout().writeString( ANSI.title( text ) );
+	/*
+	public inline function resetColor() {
+		return setColor( 39 );
+	}
+	*/
+
+	/// --- Style
+
+	public inline function bold() : Term {
+		print( CSI+''+1+'m' );
+		return this;
+	}
+
+	public inline function faint() : Term {
+		print( CSI+''+Style.faint+'m' );
+		return this;
+	}
+
+	public inline function italic() : Term {
+		print( CSI+''+Style.italic+'m' );
+		return this;
+	}
+
+	public inline function underline() : Term {
+		print( CSI+''+Style.underline+'m' );
+		return this;
+	}
+
+	public inline function reverse() : Term {
+		print( CSI+''+Style.reverse+'m' );
+		return this;
+	}
+
+	public inline function conceal() : Term {
+		print( CSI+''+Style.conceal+'m' );
+		return this;
+	}
+
+	public inline function crossed() : Term {
+		return print( CSI+''+Style.crossed_out+'m' );
+	}
+
+	/// ---
+
+	public inline function reset() {
+		return print( CSI + '0' + SUFFIX );
+	}
+
+	/// ---
+
+	public inline function println( str = '' ) : Term {
+		return print( str + '\n');
+	}
+
+	public inline function print( str : String ) : Term {
+		#if nodejs
+		js.Node.process.stdout.write( str );
+		#elseif sys
+		Sys.print( str );
+		#end
+		return this;
+	}
+
+	// ---
+
+	public static function columns() : Int {
+	    var n = Sys.getEnv( 'COLUMNS' );
+	    if( n == null )
+	        return null;
+	    return Std.parseInt(n);
     }
 
-    public static function columns() : Int {
-        var n = Sys.getEnv( 'COLUMNS' );
-        if( n == null )
-            return null;
-        return Std.parseInt(n);
-    }
+	/**
+		Try to guess if your terminal supports unicode.
+	*/
+	public static function hasUnicode() : Bool {
+		switch Sys.systemName() {
+		case 'Windows_NT':
+			return false;
+		default:
+			if( Sys.getEnv( 'LC_CTYPE' ) == 'UTF-8' )
+				return true;
+			var isUTF8 = ~/[.]UTF-8/;
+			var LC_ALL = Sys.getEnv( 'LC_ALL' );
+			if( LC_ALL != null && isUTF8.match( LC_ALL ) )
+				return true;
+			var LANG = Sys.getEnv( 'LANG' );
+			if( LANG != null && isUTF8.match( LANG ) )
+				return true;
+		}
+		return false;
+	}
 
 }
+
+#end
--- src/om/term/ANSI.hx
+++ src/om/term/ANSI.hx
@@ -1,5 +1,45 @@
 package om.term;
 
+@:enum abstract Control(String) from String to String {
+	var up = "A";
+	var down = "B";
+	var forward = "C";
+	var back = "D";
+	var nextLine = "E";
+	var previousLine = "F";
+	var horizontalAbsolute = "G";
+	var eraseData = "J";
+	var eraseLine = "L";
+	var scrollUp = "S";
+	var scrollDown = "T";
+	var savePosition = "s";
+	var restorePosition = "u";
+	var queryPosition = "6n";
+	//var hide = "?25l";
+	//var show = "?25h";
+}
+
+@:enum abstract Color(Int) from Int to Int {
+
+    var black = 30;
+    var red = 31;
+    var green = 32;
+    var yellow = 33;
+    var blue = 34;
+    var magenta = 35;
+    var cyan = 36;
+    var white = 37;
+
+    var bright_black = 90;
+    var bright_red = 91;
+    var bright_green = 92;
+    var bright_yellow = 93;
+    var bright_blue = 94;
+    var bright_magenta = 95;
+    var bright_cyan = 96;
+    var bright_white = 97;
+}
+
 @:enum abstract BackgroundColor(Int) from Int to Int {
 
     var black = 40;
@@ -21,32 +61,23 @@ package om.term;
     var bright_white = 107;
 }
 
-@:enum abstract Color(Int) from Int to Int {
-
-    var def = 39;
-
-    var black = 30;
-    var red = 31;
-    var green = 32;
-    var yellow = 33;
-    var blue = 34;
-    var magenta = 35;
-    var cyan = 36;
-    var white = 37;
-    //var Gray = 37;
-
-    var dark_grey = 90;
-
-    var bright_black = 90;
-    var bright_red = 91;
-    var bright_green = 92;
-    var bright_yellow = 93;
-    var bright_blue = 94;
-    var bright_magenta = 95;
-    var bright_cyan = 96;
-    var bright_white = 97;
+/**
+	SGR (Select Graphic Rendition)
+*/
+@:enum abstract Style(Int) from Int to Int {
+	var bold = 1;
+	var faint = 2;
+	var italic = 3;
+	var underline = 4;
+	var blink_slow = 5;
+	var blink_rapid = 6;
+	var reverse = 7;
+	var conceal = 8;
+	var crossed_out = 9;
+	//...
 }
 
+/*
 class Style {
     public static var reset = [0,22];
     public static var bright = [1,22];
@@ -56,36 +87,20 @@ class Style {
     public static var blink = [5,25];
     public static var inverse = [7,27];
 }
+*/
 
 class ANSI {
 
-    public static inline var ESCAPE = '\x1B';
-    public static inline var PREFIX = '\x1b[';
-    public static inline var SUFFIX = 'm'; // Colors only
-    public static inline var CSI = '$ESCAPE[';
-    public static inline var BELL = '\x07';
+	public static inline var ESCAPE = '\x1B';
+    public static inline var CSI = ESCAPE + '[';
+	public static inline var SUFFIX = 'm'; // Colors only
 
-	#if sys
+	public static inline var BELL = '\x07';
 
-    public static function isSupported() : Bool {
-        if( Sys.systemName() == 'Windows' )
-            return false;
-        if( Sys.getEnv( 'COLORTERM' ) != null )
-            return true;
-        var term = Sys.getEnv( 'TERM' );
-        if( term == 'dumb' )
-            return false;
-        return ~/^screen|^xterm|^vt100|color|ansi|cygwin|linux/i.match( term );
-    }
-
-	#end
-
-    /*
-    @:ansi
-	public static inline function title( text : String ) : String {
-		return '$ESCAPE]0;$text$BELL';
+	public static function color( str : String, color : Color, resetColor = 39 ) : String {
+		//return CSI + color + 'm' + str + CSI + resetColor + 'm';
+		return CSI + color + 'm';
 	}
-    */
 
     /*
     public static var REGEXP_ESCAPE_CODE(default,null) =
@@ -97,20 +112,18 @@ class ANSI {
         #end;
         */
 
-    public static function parse( str : String ) {
-        trace(str);
-        for( i in 0...str.length ) {
-            trace(str.charAt(i) +" : "+ str.charCodeAt(i));
-        }
-        return null;
-    }
-}
+	#if sys
 
-/*
-abstract ANSI(String) from String to String {
-    public inline function new( s : String ) {
-        this = s;
-    }
-}
+	public static function isSupported() : Bool {
+	    if( Sys.systemName() == 'Windows' )
+	        return false;
+	    if( Sys.getEnv( 'COLORTERM' ) != null )
+	        return true;
+	    var term = Sys.getEnv( 'TERM' );
+	    if( term == 'dumb' )
+	        return false;
+	    return ~/^screen|^xterm|^vt100|color|ansi|cygwin|linux/i.match( term );
+	}
 
-*/
+	#end
+}
