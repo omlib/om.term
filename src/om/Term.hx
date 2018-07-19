a64ac7c5748050fbef2bcf224b9fd2c9281bd3d4
tong <tong@disktree.net>
2018-07-19 14:30:52 +0200

    Cleanup

--- src/om/Term.hx
+++ src/om/Term.hx
@@ -1,11 +1,25 @@
 package om;
 
-import om.ANSI;
-import om.ANSI.CSI;
+#if macro
+import haxe.macro.Context;
+import haxe.macro.Expr;
+using haxe.macro.ExprTools;
+#end
+
+import om.ansi.BackgroundColor;
+import om.ansi.Code;
+import om.ansi.Color;
+import om.ansi.EscapeSequence;
+import om.ansi.EscapeSequence.CSI;
+import om.ansi.Style;
 
 class Term {
 
-    public static inline function print( v : Dynamic ) {
+	macro static function mkstr( rest : Array<ExprOf<String>> ) : ExprOf<String> {
+		return macro $v{ rest.map( e -> return e.getValue() ).join('') };
+	}
+
+    public static inline function print( v : String ) {
 		#if nodejs
 		js.Node.process.stdout.write( v );
 		#elseif sys
@@ -13,110 +27,99 @@ class Term {
 		#end
 	}
 
-    public static inline function println( v : Dynamic ) {
+    public static inline function println( v : String ) {
         #if nodejs
-        js.Node.process.stdout.write( v+'\n' );
+        js.Node.process.stdout.write( v+"\n" );
         #elseif sys
         std.Sys.println( v );
         #end
 	}
 
-    public static inline function printbr()
-        print( '\n' );
+	public static inline function prcsi( s : String )
+		print( CSI + s );
+
+    public static inline function prbr()
+		print( '\n' );
 
 	public static inline function beep()
-        print( ANSI.BELL );
+		print( Code.BEL );
 
 	public static inline function clear()
 		print( '\033c' );
 
 	public static inline function reset()
-		print( CSI +'0m' );
+		print( mkstr( '\x1B[', '0m' ) );
+
 
     /// Style
 
-	public static inline function background( color : BackgroundColor ) {
-		print( CSI + color + 'm' );
-	}
+	public static inline function color( color : Color = Color.default_ )
+		prcsi( color + 'm' );
 
-    public static inline function color( color : Color = Color.default_ ) {
-        print( CSI + color + 'm' );
-    }
+	public static inline function background( color : BackgroundColor = BackgroundColor.default_ )
+		prcsi( color + 'm' );
 
-	public static inline function rgb( r : Int, g : Int, b : Int ) {
-		//print( CSI + '38;5;${r};${g};${b}m' );
-		print( CSI + '38;2;${r};${g};${b}m' );
-	}
+	public static inline function rgb( r : Int, g : Int, b : Int )
+		prcsi( '38;2;${r};${g};${b}m' );
 
+	public static inline function style( attr : Array<Style> )
+		prcsi( attr.join(';') + 'm' );
+
+	/*
     public static inline function reverseVideo() {
         print( CSI + '7m' );
     }
+	*/
 
-    public static inline function style( attr : Array<Style> ) {
-        print( CSI + attr.join(';') + 'm' );
-    }
 
 	/// Display
 
-	public static inline function eraseLineToEnd() {
-		print( CSI + 'K' );
-	}
+	public static inline function eraseLineToEnd()
+		prcsi( 'K' );
 
-	public static inline function eraseLineToCursor() {
-		print( CSI + '1K' );
-	}
+	public static inline function eraseLineToCursor()
+		prcsi( '1K' );
 
-	public static inline function eraseLine() {
-		print( CSI + '2K' );
-	}
+	public static inline function eraseLine()
+		prcsi( '2K' );
 
-	public static inline function eraseDisplay() {
-		print( CSI + '2J' );
-	}
+	public static inline function eraseDisplay()
+		prcsi( '2J' );
 
-	public static inline function eraseDisplaytoCursor() {
-		print( CSI + '1J' );
-	}
+	public static inline function eraseDisplaytoCursor()
+		prcsi( '1J' );
 
-	public static inline function eraseDisplayToEnd() {
-		print( CSI + 'J' );
-	}
+	public static inline function eraseDisplayToEnd()
+		prcsi( 'J' );
+
 
     /// Cursor
 
-    public static inline function showCursor() {
-        print( CSI + '?25h' );
-    }
+    public static inline function showCursor()
+        prcsi( '?25h' );
 
-    public static inline function hideCursor() {
-        print( CSI + '?25l' );
-    }
+    public static inline function hideCursor()
+        prcsi( '?25l' );
 
-    public static inline function cursorBackward( n = 1 ) {
-        print( CSI + n + 'D' );
-    }
+    public static inline function cursorBackward( n = 1 )
+        prcsi( n + 'D' );
 
-    public static inline function cursorForward( n = 1 ) {
-        print( CSI + n + 'C' );
-    }
+    public static inline function cursorForward( n = 1 )
+        prcsi( n + 'C' );
 
-    public static inline function cursorUp( n = 1 ) {
-        print( CSI + n + 'A' );
-    }
+    public static inline function cursorUp( n = 1 )
+        prcsi( n + 'A' );
 
-    public static inline function cursorDown( n = 1 ) {
-        print( CSI + n + 'B' );
-    }
+    public static inline function cursorDown( n = 1 )
+        prcsi( n + 'B' );
 
-    public static inline function saveCursorPosition() {
-        print( CSI + 's' );
-    }
+    public static inline function saveCursorPosition()
+        prcsi( 's' );
 
-    public static inline function restoreCursorPosition() {
-        print( CSI + 'u' );
-    }
+    public static inline function restoreCursorPosition()
+        prcsi( 'u' );
 
-	/////
+	//---
 
 	public static function supportsColors() : Bool {
         if( Sys.systemName() == 'Windows' )
