cc268c35a54ca3c6bb0052b9eade038f56b12b0b
tong <tong@disktree.net>
2025-09-03 18:50:45 +0200

    add powerline symbol set

---
+++ src/om/term/symbol/Powerline.hx
@@ -0,0 +1,65 @@
+package om.term.symbol;
+
+enum abstract Powerline(String) to String {
+	// Branch / VCS
+	var branch = ""; // U+E0A0
+	var line_number = ""; // U+E0A1
+	var lock = ""; // U+E0A2
+	var column_number = ""; // U+E0A3
+
+	// Separators (arrows)
+	var right_arrow = ""; // U+E0B0
+	var right_arrow_thin = ""; // U+E0B1
+	var left_arrow = ""; // U+E0B2
+	var left_arrow_thin = ""; // U+E0B3
+
+	// Separators (rounded)
+	var right_round = ""; // U+E0B4
+	var right_round_thin = ""; // U+E0B5
+	var left_round = ""; // U+E0B6
+	var left_round_thin = ""; // U+E0B7
+
+	// Extra separators
+	var right_half = ""; // U+E0B8
+	var right_half_thin = ""; // U+E0B9
+	var left_half = ""; // U+E0BA
+	var left_half_thin = ""; // U+E0BB
+	var right_split = ""; // U+E0BC
+	var right_split_thin = ""; // U+E0BD
+	var left_split = ""; // U+E0BE
+	var left_split_thin = ""; // U+E0BF
+
+	// Triangles block (U+E0C0 – U+E0DF)
+	var triangle0 = ""; // U+E0C0
+	var triangle1 = ""; // U+E0C1
+	var triangle2 = ""; // U+E0C2
+	var triangle3 = ""; // U+E0C3
+	var triangle4 = ""; // U+E0C4
+	var triangle5 = ""; // U+E0C5
+	var triangle6 = ""; // U+E0C6
+	var triangle7 = ""; // U+E0C7
+	var triangle8 = ""; // U+E0C8
+	var triangle9 = ""; // U+E0C9
+	var triangle10 = ""; // U+E0CA
+	var triangle11 = ""; // U+E0CB
+	var triangle12 = ""; // U+E0CC
+	var triangle13 = ""; // U+E0CD
+	var triangle14 = ""; // U+E0CE
+	var triangle15 = ""; // U+E0CF
+	var triangle16 = ""; // U+E0D0
+	var triangle17 = ""; // U+E0D1
+	var triangle18 = ""; // U+E0D2
+	var triangle19 = ""; // U+E0D3
+	var triangle20 = ""; // U+E0D4
+	var triangle21 = ""; // U+E0D5
+	var triangle22 = ""; // U+E0D6
+	var triangle23 = ""; // U+E0D7
+	var triangle24 = ""; // U+E0D8
+	var triangle25 = ""; // U+E0D9
+	var triangle26 = ""; // U+E0DA
+	var triangle27 = ""; // U+E0DB
+	var triangle28 = ""; // U+E0DC
+	var triangle29 = ""; // U+E0DD
+	var triangle30 = ""; // U+E0DE
+	var triangle31 = ""; // U+E0DF
+}
